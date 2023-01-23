942dacf21e0109dcd6e22cf763c4d7dcaae6d06d
mohamedmek <mohamed_abdelkarim@protonmail.com>
2023-01-23 13:28:42 +0200

    lab3

--- Day3/lab/validation.php
+++ Day3/lab/validation.php
@@ -20,34 +20,38 @@
                         else{
                                 $email=$_POST['email'];
                             }
+
                         if(empty($_POST['gender'])){
                                 $genderErr= "gender is required";
                         }
                         else{
                             $gender=$_POST['gender'];
                         }
+                        
                         if(empty($_POST['terms'])){
                             $termsErr= "You must agree to the terms of use";
                         }
-                        
+                        else{
                             $terms=$_POST['terms'];
+                            echo "thanks for entering your data :)";
+                        }
 
-                            function showData(){
+                        function showData(){
                             
-                            echo "<h2>Your given values are as:</h2>"; 
-                            echo"<br><br>";
-                            echo "Name: " . $name;
-                            echo"<br>";
-                            echo "E-mail: " . $email;
-                            echo"<br>";
-                            echo "Group #: " . $_POST['group'];
-                            echo"<br>";
-                            echo "Class details: " . $_POST['class_details'];
-                            echo"<br>";
-                            echo "Gender: " . $gender;
-                            echo"<br>";
-                            echo "Your courses are: " . $_POST['courses'];
-                            echo"<br>"; 
+                                echo "<h2>Your given values are as:</h2>"; 
+                                echo"<br><br>";
+                                echo "Name: " . $name;
+                                echo"<br>";
+                                echo "E-mail: " . $email;
+                                echo"<br>";
+                                echo "Group #: " . $_POST['group'];
+                                echo"<br>";
+                                echo "Class details: " . $_POST['class_details'];
+                                echo"<br>";
+                                echo "Gender: " . $gender;
+                                echo"<br>";
+                                echo "Your courses are: " . $_POST['courses'];
+                                echo"<br>"; 
                             }
                         
                         }
